8110dbce664a2bd9459f66810366c21a8428222c
smrtsmrf <smrtsmrf@gmail.com>
2017-01-30 19:54:00 -0700

    finished chapter 4. this is neat.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "my-app",
-  template: `
-    <StackLayout>
-      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
-      <TextField hint="Password" secure="true"></TextField>
-
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
-    </StackLayout>
-  `,
-  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+  selector: "main",
+  template: "<page-router-outlet></page-router-outlet>"
 })
 export class AppComponent {}
---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { Page } from "ui/page";
+import { Color } from "color";
+import { View } from "ui/core/view";
+
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
+  providers: [UserService]
+})
+export class LoginComponent implements OnInit {
+  user: User;
+  isLoggingIn: boolean = true;
+
+  @ViewChild("container") container: ElementRef;
+
+  constructor(private router: Router, private userService: UserService, private page: Page) {
+    this.user = new User();
+  }
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    }
+    else {
+      this.signUp();
+    }
+  }
+
+  login() {
+    this.userService.login(this.user)
+        .subscribe(
+            () => this.router.navigate(["/list"]),
+            (error) => alert("Unfortunately we could not find your account.")
+        );
+  }
+
+  signUp() {
+    this.userService.register(this.user)
+        .subscribe(
+            () => {
+              alert("Your account was successfully created.");
+              this.toggleDisplay();
+            },
+            () => alert("Unfortunately we were unable to create your account.")
+        );
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+      duration: 200
+    });
+  }
+
+}
